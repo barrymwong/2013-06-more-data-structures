02d94b6f70c8433ca0fe2371bbf665b639047ce4
Hack Reactor Students <github@hackreactor.com>
2013-06-22 11:32:00 -0700

    extended methods to tree object

--- src/tree.js
+++ src/tree.js
@@ -9,16 +9,18 @@
 // reflecting whether it can be found as the value of the
 // target node or any descendant node
 
-var makeTree = function(){
+var makeTree = function(val){
   var newTree = {};
-  Tree.value = undefined;
-
+  newTree.value = val;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(val){
+  makeTree(val);
 };
 
 treeMethods.contains = function(){
@@ -27,15 +29,15 @@ treeMethods.contains = function(){
 
 /*
 1 Be able to add an initial node.
-   Create a new object that contains a value property and a chlidren property.
+   Create a new object that contains a value property and a children property.
      The value property should contain a passed in value.
-     The next property should equal an empty array.
+     The children property should equal an empty array.
 
 2 Then add child node to the parent node's children property.
     Repeat step 1 to create new child node.
     Push the new child node to the empty array in the parent's next property
 3 Find a specific value in the node tree
-  recursively look for the value similiarly to linked list 
+  recursively look for the value similiarly to linked list
     iterate over the first element in the children array and recurse down the tree
     iterate over the next element in the children array down the tree
     etc.
