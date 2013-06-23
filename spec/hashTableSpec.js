9a02cbac49c8ba8e3e6a5977a7d183b903675fc0
Hack Reactor Students <github@hackreactor.com>
2013-06-23 15:41:42 -0700

    redid remove test

--- spec/hashTableSpec.js
+++ spec/hashTableSpec.js
@@ -1,34 +1,27 @@
 describe("hashTable", function() {
-  var hashTable;
+  var vet;
 
   beforeEach(function() {
-    hashTable = new HashTable();
+    vet = new HashTable();
   });
 
   it("should have methods named 'insert' and 'retrieve", function() {
-    expect(hashTable.insert).toEqual(jasmine.any(Function));
-    expect(hashTable.retrieve).toEqual(jasmine.any(Function));
+    expect(vet.insert).toEqual(jasmine.any(Function));
+    expect(vet.retrieve).toEqual(jasmine.any(Function));
   });
 
-  it("insert a key and value", function() {
-    hashTable.insert('greeting', 'hey');
-    hashTable.insert('formal greeting', 'hello');
-    expect(hashTable.retrieve('greeting')).toEqual('hey');
-    expect(hashTable.retrieve('formal greeting')).toEqual('hello');
+  it("returns the value inserted at a given key, when retrieving from that same key", function() {
+    vet.insert('greeting', 'hey');
+    vet.insert('formal greeting', 'hello');
+    expect(vet.retrieve('greeting')).toEqual('hey');
+    expect(vet.retrieve('formal greeting')).toEqual('hello');
   });
 
-  it("use retrieve to find a value", function() {
-    hashTable.insert('greeting', 'hey');
-    hashTable.insert('formal greeting', 'hello');
-    expect(hashTable.retrieve('formal greeting')).toEqual('hello');
+  it("returns undefined when retrieving a key that was previously removed", function() {
+    vet.insert('greeting', 'hey');
+    vet.remove('greeting');
+    expect(vet.retrieve('greeting')).toEqual(undefined);
   });
 
-  it("should remove an element", function() {
-    hashTable.insert('hey'); // 6
-    hashTable.insert('hello'); // 4
-    hashTable.remove('hello');
-    expect(hashTable.retrieve(4)).toEqual('hello');
-  });
-
-  // add more tests here to test the functionality of hashTable
+  // add more tests here to test the functionality of vet
 });
